784f7cc4599abfd1c9ef81852c2c5ce705f9b039
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2021-08-14 23:14:08 +0200

    more information visible

--- public/public/index.js
+++ public/public/index.js
@@ -10,4 +10,6 @@ fakeConsole.start()
     .typeString('<h2></h2><a href="https://stackoverflow.com/users/14491660/eliouz">>StackOverflow<span></span></a>')
     .typeString('  :)')
     .pauseFor(200)
-    .deleteChars(4);
+    .deleteChars(4)
+    .pauseFor(2000)
+    .typeString("<h2></h2><h2></h2>>I'm still learning new things every weeks, be kind on what I've done so far ;)");
